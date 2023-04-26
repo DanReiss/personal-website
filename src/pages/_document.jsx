40047ce22caaa8b60a3f45a0b07ece803cfc8dc5
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-04-26 12:55:33 -0300

    base do projeto

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head title='Danilo dos Reis Amaral'/>
       <body>
         <Main />
         <NextScript />
