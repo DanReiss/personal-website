6c4d956f1e80b6a1a1452db7a46062155e51fcb2
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-05-03 17:42:54 -0300

    adicionar key para cada div de projeto

--- src/components/ProjectsCarousel.jsx
+++ src/components/ProjectsCarousel.jsx
@@ -9,8 +9,8 @@ function ProjectsCarousel({ jsondata }) {
       {/* Container */}
       <div className="width-full flex flex-row align-center overflow-auto gap-3">
         {
-          projects.map((project) => (
-            <div className="shrink-0 w-[145px] h-[215px] bg-blue">
+          projects.map((project, i) => (
+            <div key={i} className="shrink-0 w-[145px] h-[215px] bg-blue">
               <div className="grid place-items-end w-full h-full bg-dark_blue/50">
                 <h4 className="w-full font-roboto text-center uppercase tracking-[0.2em] text-[0.9em] mb-1">{project.name}</h4>
               </div>
