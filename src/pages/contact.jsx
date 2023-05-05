bad03bd8eb9ab81b79b93ca8c53b16bc518741c9
Danilo dos Reis Amaral <danreis.ctt@gmail.com>
2023-05-05 14:29:27 -0300

    ativar formulário
    O formulário de contato agora está funcionando por meio do 'formsubmit.co' que automatiza todo o processo de envio de formulários

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -17,22 +17,22 @@ function Contact() {
           responderei o mais rápido que eu conseguir.
         </p>
         <div className="w-full bg-gray px-3 py-4 mt-8">
-          <form action="">
+          <form action="https://formsubmit.co/c4baf9a12205d942d5f8de171813f333" method="POST">
             <label className="block h-full mb-2">
               Assunto
-              <input type="text" className="w-full text-dark_blue p-1" />
+              <input type="text" name="about" required className="w-full text-dark_blue p-1" />
             </label>
             <label className="block h-full mb-2">
               Email
-              <input type="text" className="w-full text-dark_blue p-1" />
+              <input type="text" name="email" required className="w-full text-dark_blue p-1" />
             </label>
             <label className="block h-full mb-2">
               Nome
-              <input type="text" className="w-full text-dark_blue p-1" />
+              <input type="text" name="name" required className="w-full text-dark_blue p-1" />
             </label>
             <label className="block h-full mb-2">
               Mensagem
-              <textarea cols="30" rows="10" className="w-full text-dark_blue p-1" />
+              <textarea cols="30" rows="10" name="message" required className="w-full text-dark_blue p-1" />
             </label>
             <button type="submit" className="block m-auto bg-blue px-10 py-1">Enviar</button>
           </form>
